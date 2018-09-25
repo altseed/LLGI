1ab68973ccb26bf4a546e868541039a9938061a6
durswd <swda.durl@gmail.com>
2018-09-26 02:43:18 +0900

    Implementing

--- src/G3/LLGI.G3.Graphics.h
+++ src/G3/LLGI.G3.Graphics.h
@@ -8,6 +8,14 @@ namespace LLGI
 namespace G3
 {
 
+class RenderTarget
+	: public ReferenceObject
+{
+public:
+	RenderTarget() = default;
+	virtual ~RenderTarget() = default;
+};
+
 class Graphics
 	: public ReferenceObject
 {
@@ -18,6 +26,13 @@ public:
 
 	virtual void Execute(CommandList* commandList);
 
+	/**
+		@brief get render target of screen to show on a display.
+		@note
+		Don't release and addref it.
+		Don't use it for the many purposes, please input Clear or SetRenderTarget immediately.
+	*/
+	virtual RenderTarget* GetCurrentScreen();
 	virtual VertexBuffer* CreateVertexBuffer(int32_t size);
 	virtual IndexBuffer* CreateIndexBuffer(int32_t stride, int32_t count);
 	virtual Shader* CreateShader(DataStructure* data, int32_t count);
