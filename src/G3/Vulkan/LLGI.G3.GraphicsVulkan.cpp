6394c7aee063514c72c973853f9a8e81acddd2fa
durswd <swda.durl@gmail.com>
2019-02-25 10:12:59 +0900

    Added templates

--- src/G3/Vulkan/LLGI.G3.GraphicsVulkan.cpp
+++ src/G3/Vulkan/LLGI.G3.GraphicsVulkan.cpp
@@ -5,7 +5,44 @@ namespace LLGI
 namespace G3
 {
 
+GraphicsVulkan::GraphicsVulkan(int32_t swapBufferCount) : swapBufferCount_(swapBufferCount) {}
 
+GraphicsVulkan::~GraphicsVulkan() {}
 
+void GraphicsVulkan::NewFrame() { throw "Not inplemented"; }
+
+void GraphicsVulkan::SetWindowSize(const Vec2I& windowSize) { throw "Not inplemented"; }
+
+void GraphicsVulkan::Execute(CommandList* commandList) { throw "Not inplemented"; }
+
+void GraphicsVulkan::WaitFinish() { throw "Not inplemented"; }
+
+RenderPass* GraphicsVulkan::GetCurrentScreen(const Color8& clearColor, bool isColorCleared, bool isDepthCleared)
+{
+	throw "Not inplemented";
 }
+
+VertexBuffer* GraphicsVulkan::CreateVertexBuffer(int32_t size) { throw "Not inplemented"; }
+
+IndexBuffer* GraphicsVulkan::CreateIndexBuffer(int32_t stride, int32_t count) { throw "Not inplemented"; }
+
+Shader* GraphicsVulkan::CreateShader(DataStructure* data, int32_t count) { throw "Not inplemented"; }
+
+PipelineState* GraphicsVulkan::CreatePiplineState() { throw "Not inplemented"; }
+
+CommandList* GraphicsVulkan::CreateCommandList() { throw "Not inplemented"; }
+
+ConstantBuffer* GraphicsVulkan::CreateConstantBuffer(int32_t size, ConstantBufferType type = ConstantBufferType::LongTime)
+{
+	throw "Not inplemented";
+}
+RenderPass* GraphicsVulkan::CreateRenderPass(const Texture** textures, int32_t textureCount, Texture* depthTexture)
+{
+	throw "Not inplemented";
+}
+Texture* GraphicsVulkan::CreateTexture(const Vec2I& size, bool isRenderPass, bool isDepthBuffer) { throw "Not inplemented"; }
+
+Texture* GraphicsVulkan::CreateTexture(uint64_t id) { throw "Not inplemented"; }
+
+} // namespace G3
 } // namespace LLGI
--- src/G3/Vulkan/LLGI.G3.GraphicsVulkan.h
+++ src/G3/Vulkan/LLGI.G3.GraphicsVulkan.h
@@ -9,14 +9,41 @@ namespace G3
 
 class RenderPassVulkan : public RenderPass
 {
+};
 
+class TempMemoryPool
+{
+public:
 };
 
 class GraphicsVulkan : public Graphics
 {
 private:
-public:
+	int32_t swapBufferCount_ = 0;
 
+public:
+	GraphicsVulkan(int32_t swapBufferCount);
+
+	virtual ~GraphicsVulkan();
+
+	void NewFrame() override;
+
+	void SetWindowSize(const Vec2I& windowSize) override;
+
+	void Execute(CommandList* commandList) override;
+
+	void WaitFinish() override;
+
+	RenderPass* GetCurrentScreen(const Color8& clearColor, bool isColorCleared, bool isDepthCleared) override;
+	VertexBuffer* CreateVertexBuffer(int32_t size) override;
+	IndexBuffer* CreateIndexBuffer(int32_t stride, int32_t count) override;
+	Shader* CreateShader(DataStructure* data, int32_t count) override;
+	PipelineState* CreatePiplineState() override;
+	CommandList* CreateCommandList() override;
+	ConstantBuffer* CreateConstantBuffer(int32_t size, ConstantBufferType type = ConstantBufferType::LongTime) override;
+	RenderPass* CreateRenderPass(const Texture** textures, int32_t textureCount, Texture* depthTexture) override;
+	Texture* CreateTexture(const Vec2I& size, bool isRenderPass, bool isDepthBuffer) override;
+	Texture* CreateTexture(uint64_t id) override;
 };
 
 } // namespace G3
