68d5764945d38034c84bcf75ccb314b1a23f7366
durswd <swda.durl@gmail.com>
2020-06-23 02:34:02 +0900

    Update Vulkan shaders

--- src_test/Shaders/SPIRV/convert.bat
+++
@@ -1,7 +0,0 @@
-glslangValidator ..\GLSL\simple_constant_rectangle.vert -e main -V -l -o simple_constant_rectangle.vert.spv
-glslangValidator ..\GLSL\simple_constant_rectangle.frag -e main -V -l -o simple_constant_rectangle.frag.spv
-
-glslangValidator ..\GLSL\simple_texture_rectangle.vert -e main -V -l -o simple_texture_rectangle.vert.spv
-glslangValidator ..\GLSL\simple_texture_rectangle.frag -e main -V -l -o simple_texture_rectangle.frag.spv
-
-glslangValidator ../GLSL/simple_mrt_texture_rectangle.frag -e main -V -l -o simple_mrt_texture_rectangle.frag.spv
