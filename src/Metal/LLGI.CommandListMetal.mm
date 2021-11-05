9e5c91fa8b70eda3b2309b19c606c52cc857c13a
durswd <swda.durl@gmail.com>
2021-11-05 23:30:16 +0900

    Fix for ios

--- src/Metal/LLGI.CommandListMetal.mm
+++ src/Metal/LLGI.CommandListMetal.mm
@@ -360,7 +360,7 @@ void CommandListMetal::BeginRenderPass(RenderPass* renderPass)
 			rpd.depthAttachment.clearDepth = 1.0;
 
 			if (rp->depthStencilFormat != MTLPixelFormatDepth32Float_Stencil8
-#if TARGET_OS_MAC
+#if !(TARGET_OS_IPHONE) && !(TARGET_OS_SIMULATOR)
 				&& rp->depthStencilFormat != MTLPixelFormatDepth24Unorm_Stencil8
 #endif
 			)
