6e40e635fc5f75308ad05d2c70c4edcab845d242
durswd <swda.durl@gmail.com>
2023-11-17 21:47:37 +0900

    Make a transpiler script support Linux

--- scripts/transpile.py
+++ scripts/transpile.py
@@ -12,10 +12,20 @@ args = aparser.parse_args()
 target_directory=os.path.join(os.getcwd(), args.target)
 os.chdir(os.path.dirname(__file__))
 
-transpiler_filename = "ShaderTranspiler.exe" if platform.system() == 'Windows' else "ShaderTranspiler"
+transpiler_filename = "ShaderTranspiler"
+if platform.system() == 'Windows':
+    transpiler_filename += ".exe"
+
 transpiler_path = os.path.join("../build/tools/ShaderTranspiler/Debug", transpiler_filename)
+transpiler_path_make = os.path.join("../build/tools/ShaderTranspiler", transpiler_filename)
 if os.path.isfile(transpiler_path):
     shutil.copy(transpiler_path, ".")
+elif os.path.isfile(transpiler_path_make):
+    shutil.copy(transpiler_path_make, "./")
+
+transpiler_call = 'ShaderTranspiler'
+if platform.system() == 'Linux':
+    transpiler_call = './ShaderTranspiler'
 
 verts = glob.glob(os.path.join(target_directory, 'HLSL_DX12/*.vert'), recursive=True)
 frags = glob.glob(os.path.join(target_directory, 'HLSL_DX12/*.frag'), recursive=True)
@@ -30,11 +40,12 @@ for target,directory in [
         ('--frag', frags),
         ('--comp', comps) ]:
         for f in paths:
-            subprocess.call(['ShaderTranspiler', kind, target, '--input', f, '--output', os.path.join(target_directory, directory, os.path.basename(f))])
+            subprocess.call([transpiler_call, kind, target, '--input', f, '--output', os.path.join(target_directory, directory, os.path.basename(f))])
 
 verts = glob.glob(os.path.join(target_directory, 'GLSL_VULKAN/*.vert'), recursive=True)
 frags = glob.glob(os.path.join(target_directory, 'GLSL_VULKAN/*.frag'), recursive=True)
 comps = glob.glob(os.path.join(target_directory, 'GLSL_VULKAN/*.comp'), recursive=True)
 
-for f in (verts + frags + comps):
-    subprocess.call(['glslangValidator', f, '-e', 'main', '-V', '-o', os.path.join(target_directory, 'SPIRV', os.path.basename(f)) + '.spv'])
+if platform.system() != 'Linux':
+    for f in (verts + frags + comps):
+        subprocess.call(['glslangValidator', f, '-e', 'main', '-V', '-o', os.path.join(target_directory, 'SPIRV', os.path.basename(f)) + '.spv'])
